bb859db9fb084e48083085e82b5bfaf2c49e99c3
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-05-09 12:55:34 +0300

    lesson1

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,11 +1,97 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+
+interface Item { 
+    id:string,
+    title: string
+}
 
 const AboutTab = () => {
-    return(
-        <View>
-            <Text>About Tab</Text>
+    // const data = [
+    //     { id: '1', title: 'item1' },
+    //     { id: '2', title: 'item2' },
+    //     { id: '3', title: 'item3' },
+    //     { id: '4', title: 'item4' },
+    //     { id: '5', title: 'item5' },
+    //     { id: '6', title: 'item6' },
+    // ];
+    const data = [] as Item[]
+    return (
+        <View style={styles.container}>
+            <View style={styles.card}>
+                <FlatList
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <View style={styles.listItem}>
+                            <Text style={styles.listItemText}>{item.title}</Text>
+                        </View>
+                    )}
+                    ListEmptyComponent={
+                        <Text style={{
+                            textAlign:'center',
+                            marginTop: 50,
+                            color: '#8e8e93',
+                            fontSize: 16
+                        }}>List is empty</Text>
+                    }
+                />
+            </View>
         </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 20, gap: 15 },
+    card: {
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 15,
+        elevation: 4,
+        shadowColor: '#000',
+        shadowOpacity: 0.9,
+        shadowRadius: 10,
+        gap: 10
+    },
+    title: { fontSize: 22, fontWeight: 'bold', fontFamily: 'sans-serif' },
+    desc: { fontSize: 16, color: '#666' },
+    button: {
+        backgroundColor: '#007AFF',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center'
+    },
+    buttonText: { color: '#ffffff', fontSize: 16, fontWeight: '600' },
+    switch: {
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 20,
+        elevation: 4,
+        shadowColor: '#000',
+        shadowOpacity: 0.9,
+        shadowRadius: 10,
+        borderRadius: 15
+    },
+    input: {
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        paddingHorizontal: 15,
+        fontSize: 16
+    },
+    listItem: {
+        backgroundColor: "#f9f9f9",
+        padding: 20,
+        borderRadius: 10,
+        borderLeftWidth: 5,
+        borderLeftColor: '#007aff',
+        marginVertical: 8
+    },
+    listItemText: {fontSize: 16},
+
+})
+
 export default AboutTab;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,35 @@
-import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import React, {useState} from 'react';
+import {
+    View, 
+    Text, 
+    Switch, 
+    StyleSheet, 
+    ToastAndroid,
+    TouchableOpacity, 
+    Alert,
+    Button,
+    TextInput,
+    FlatList,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = ()=>{
+    const [isEnabled, setIsEnabled] = useState(false)
+    const [text, setText] = useState('')
 
+   
+    
     const pressButton = ()=>{
         Alert.alert("Thanks for Subscribe", 'Waiting GooglePay services', [
             {
                 text:'Ok',
-                onPress:()=>console.log('Ok Pressed')
+                onPress:()=>console.log('Ok Pressed'),
+                style: 'cancel'
             }
         ] )
+    }
+    const showToast = ()=>{
+        ToastAndroid.show("Toast!", ToastAndroid.LONG)
     }
 
     return(
@@ -21,6 +41,31 @@ const HomeScreen = ()=>{
             <TouchableOpacity onPress={pressButton} style={styles.button}>
                 <Text style={styles.buttonText}>Subscribe $2.99/Month</Text>
             </TouchableOpacity>
+            <TouchableOpacity onPress={showToast} style={[styles.button ,{ backgroundColor: '#1b9c2c'}]}>
+                <Text style={styles.buttonText}>Show Toast</Text>
+            </TouchableOpacity>
+
+            <Button title='Default button' color={'#0f836a'}/>
+
+            <View style={styles.switch}>
+                <Text style={{fontSize: 18}}>Notifications</Text>
+                <Switch 
+                trackColor={{'false':'#767577', true:'#81b0ff'}}
+                thumbColor={isEnabled ? '#007aff' : '#f4f3f4'}
+                onValueChange={() => setIsEnabled(!isEnabled)}
+                value={isEnabled}
+                />
+            </View>
+            <View style={styles.card}>
+                <TextInput
+                    style={styles.input} 
+                    placeholder='Type something'
+                    onChangeText={setText}
+                    value={text}
+                    placeholderTextColor="#999"
+                />
+                <Text style={{color:'gray', fontSize: 18}}>&gt; {text}</Text>
+            </View>
         </View>
     )
 }
@@ -50,16 +95,46 @@ const styles = StyleSheet.create({
         elevation: 4,
         shadowColor:'#000',
         shadowOpacity: 0.9,
-        shadowRadius: 10
+        shadowRadius: 10,
+        gap: 10
     },
     title:{fontSize: 22, fontWeight:'bold', fontFamily:'sans-serif'},
-    desc:{fontSize:16, color:'#666', marginTop: 10},
+    desc:{fontSize:16, color:'#666'},
     button:{
         backgroundColor:'#007AFF',
         padding: 15,
         borderRadius: 10,
         alignItems: 'center'
     },
-    buttonText: {color:'#ffffff', fontSize: 16, fontWeight:'600'}
+    buttonText: {color:'#ffffff', fontSize: 16, fontWeight:'600'},
+    switch:{
+        backgroundColor:'#fff',
+        flexDirection: 'row',
+        alignItems:'center',
+        justifyContent: 'space-between',
+        padding: 20,
+        elevation: 4,
+        shadowColor:'#000',
+        shadowOpacity: 0.9,
+        shadowRadius: 10,
+        borderRadius: 15
+    },
+    input:{
+        height:50,
+        borderWidth:1,
+        borderColor:'#ccc',
+        borderRadius:8,
+        paddingHorizontal: 15,
+        fontSize: 16
+    },
+    listItem:{
+        backgroundColor:"#f9f9f9",
+        padding: 20,
+        borderRadius: 10,
+        borderLeftWidth: 5,
+        borderLeftColor: '#007aff'
+    },
+    listItemText:{},
+
 })
 export default HomeScreen;
